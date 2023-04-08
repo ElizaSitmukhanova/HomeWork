af85fe5e35701b75e967373d7e8d6c41d8922151
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-08 21:41:24 +0300

    first version

--- homework.js
+++ homework.js
@@ -1,50 +1,23 @@
-  "use strict";
+"use strict";
 
-const numberOfFilms=+prompt ('Сколько фильмов вы уже посмотрели?','');
-function start() {
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-}
-const personalMovieDB = {
+let personalMovieDB = {
      count: numberOfFilms,
      movies: {},
      actors: {},
      genres: [],
-     privat: false
+     privat: false,
 };
-     
-for (let i=0; i< 2; i++){
-     const oneQn=prompt('Один из последних просмотренных фильмов', ''),
-           twoQn=prompt('На сколько оцените его', '');
-      if (oneQn!=null && twoQn != null && oneQn!= "" && twoQn != "" && oneQn.length > 5) {
-          personalMovieDB.movies[oneQn]  =twoQn;
-          console.log('done');
-      } else {
-          console.log('error');
-          i--;
-      }
 
-     }
-
-console.log(personalMovieDB);
-
-if (personalMovieDB.count < 10){
-     console.log('You watched so little films');
-} else if (personalMovieDB.count>=10 && personalMovieDB.count<=30) {
-     console.log('You are classical viewer');
-} else if (personalMovieDB.count>30) {
-     console.log('You are cinephile');
-} else {
-     console.log('Error');
-}
- 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('Насколько оцените?',''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('Насколько оцените?','');
 
 
-function showFirstMessage(text) {
-     let a = console.log(text)
-     return a;
-     
-}
-showFirstMessage('Hello world!');
+    personalMovieDB.movies[a]=b;
+    
+    personalMovieDB.movies[c]=d;
+    console.log(personalMovieDB);
 
-
- 
