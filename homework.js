d3e2f487ec6ba476272a6dcf241e39e4ac6cc9d7
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-04-09 11:30:59 +0300

    second task

--- homework.js
+++ homework.js
@@ -10,14 +10,26 @@ let personalMovieDB = {
      privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('Насколько оцените?',''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('Насколько оцените?','');
+for (let i = 0; i < 2; i++) {
+     const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('Насколько оцените?', '');
+          if ( a != null && b != null && a != '' && b != "" && a.length < 50) {
+                    personalMovieDB.movies[a] = b;
+                    console.log('done');
+          } else {
+               console.log("error");
+               i--
+          }
+}
 
-
-    personalMovieDB.movies[a]=b;
-    
-    personalMovieDB.movies[c]=d;
-    console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+     console.log('Просмотрено довольно много фильмв');
+} else if  (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+     console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+     console.log("Вы киноман");
+} else {
+     console.log('Произошла ошибка');
+}
+console.log(personalMovieDB);
 
